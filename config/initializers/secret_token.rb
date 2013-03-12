ddbcf46e53c6e431626ee38e22194de0eccd1143
datpt91 <datpt91@gmail.com>
2013-03-12 14:14:29 +0700

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zapp::Application.config.secret_token = '63349d27b71fb25c15528dd32d5a1f54cf9cf7ebf5b0a6d313eae8e8fa282df158ec6bbf7a8df048213af1a46cfa79ca3b7047e5a1a6268fe173c90a3ffad13f'

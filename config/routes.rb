d0d197dc1e642d85a480472871f985faa97ef842
datpt91 <datpt91@gmail.com>
2013-03-12 16:19:52 +0700

    basic model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Zapp::Application.routes.draw do
+  
+  get "users/new"
+
+  root to: 'static_pages#home'
+
+  match '/about',   to: 'static_pages#about'
+  match '/signup',  to: 'users#new'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
